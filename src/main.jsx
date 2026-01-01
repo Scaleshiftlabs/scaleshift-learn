1b7c31a6dde6a7edda4cdcaa432aa555e85681fd
Shivakumar <shivakumar@Shivakumars-MacBook-Air.local>
2026-01-01 20:53:16 +0530

    fix: vercel rewrite to stop forced /course redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 
@@ -9,9 +9,9 @@ const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkKey}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </ClerkProvider>
   </React.StrictMode>
 );
