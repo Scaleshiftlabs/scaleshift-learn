fa2956d37d241b1df32ff597bb3f522e4397f055
Shivakumar <shivakumar@Shivakumars-MacBook-Air.local>
2026-01-01 21:08:25 +0530

    fix: enable nested routes with Outlet for assignment and quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+
 import CourseHome from "./pages/course/CourseHome";
 import Module from "./pages/course/modules/Module";
 import Video from "./pages/course/modules/Video";
@@ -9,10 +10,12 @@ export default function App() {
   return (
     <Routes>
       <Route path="/course" element={<CourseHome />} />
-      <Route path="/course/module/:id" element={<Module />} />
-      <Route path="/course/module/:id/video/:vid" element={<Video />} />
-      <Route path="/course/module/:id/assignment" element={<Assignment />} />
-      <Route path="/course/module/:id/quiz" element={<Quiz />} />
+
+      <Route path="/course/module/:id" element={<Module />}>
+        <Route path="video/:vid" element={<Video />} />
+        <Route path="assignment" element={<Assignment />} />
+        <Route path="quiz" element={<Quiz />} />
+      </Route>
     </Routes>
   );
 }
--- src/pages/course/modules/Module.jsx
+++ src/pages/course/modules/Module.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, Outlet } from "react-router-dom";
 
 export default function Module() {
   const { id } = useParams();
@@ -18,51 +18,34 @@ export default function Module() {
 
       <ul style={{ lineHeight: "2", marginTop: 24 }}>
         <li>
-          <Link
-            to={`/course/module/${id}/video/1`}
-            style={{ color: "#38bdf8" }}
-          >
+          <Link to="video/1" style={{ color: "#38bdf8" }}>
             ▶ Video 1
           </Link>
         </li>
-
         <li>
-          <Link
-            to={`/course/module/${id}/video/2`}
-            style={{ color: "#38bdf8" }}
-          >
+          <Link to="video/2" style={{ color: "#38bdf8" }}>
             ▶ Video 2
           </Link>
         </li>
-
         <li>
-          <Link
-            to={`/course/module/${id}/assignment`}
-            style={{ color: "#38bdf8" }}
-          >
+          <Link to="assignment" style={{ color: "#38bdf8" }}>
             📝 Assignment
           </Link>
         </li>
-
         <li>
-          <Link
-            to={`/course/module/${id}/quiz`}
-            style={{ color: "#38bdf8" }}
-          >
+          <Link to="quiz" style={{ color: "#38bdf8" }}>
             ❓ Quiz
           </Link>
         </li>
       </ul>
 
+      {/* 👇 THIS IS THE MISSING PIECE */}
       <div style={{ marginTop: 40 }}>
-        <Link
-          to="/course"
-          style={{
-            color: "#a5b4fc",
-            textDecoration: "none",
-            fontSize: 16
-          }}
-        >
+        <Outlet />
+      </div>
+
+      <div style={{ marginTop: 40 }}>
+        <Link to="/course" style={{ color: "#a5b4fc" }}>
           ← Back to Course
         </Link>
       </div>
