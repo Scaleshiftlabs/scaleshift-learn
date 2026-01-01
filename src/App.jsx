4a78b06455264123b70e3a16ca610bde9013dcaa
Shivakumar <shivakumar@Shivakumars-MacBook-Air.local>
2026-01-01 20:38:18 +0530

    fix: make course modules publicly accessible

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { SignedIn, SignedOut, SignIn } from "@clerk/clerk-react";
+import { SignedOut, SignIn } from "@clerk/clerk-react";
 import CourseHome from "./pages/course/CourseHome";
 import Module from "./pages/course/modules/Module";
 import Video from "./pages/course/modules/Video";
@@ -7,14 +7,21 @@ import Video from "./pages/course/modules/Video";
 export default function App() {
   return (
     <Routes>
+      {/* Home */}
       <Route path="/" element={<Navigate to="/course" replace />} />
 
-      <Route path="/sign-in/*" element={<SignedOut><SignIn routing="path" path="/sign-in" /></SignedOut>} />
+      {/* Auth */}
+      <Route
+        path="/sign-in/*"
+        element={<SignedOut><SignIn routing="path" path="/sign-in" /></SignedOut>}
+      />
 
+      {/* Course (PUBLIC for now) */}
       <Route path="/course" element={<CourseHome />} />
-      <Route path="/course/module/:id" element={<SignedIn><Module /></SignedIn>} />
-      <Route path="/course/module/:id/video/:vid" element={<SignedIn><Video /></SignedIn>} />
+      <Route path="/course/module/:id" element={<Module />} />
+      <Route path="/course/module/:id/video/:vid" element={<Video />} />
 
+      {/* Fallback */}
       <Route path="*" element={<Navigate to="/course" replace />} />
     </Routes>
   );
