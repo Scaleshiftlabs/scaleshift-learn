acc3e561bfe0e97c7d32d1fdbf637fb0b8a5ac75
Shivakumar <shivakumar@Shivakumars-MacBook-Air.local>
2026-01-01 23:54:52 +0530

    fix: stop root redirect and add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CourseHome from "./pages/course/CourseHome";
 import Module from "./pages/course/modules/Module";
 import Video from "./pages/course/modules/Video";
@@ -6,10 +6,20 @@ import Assignment from "./pages/course/modules/Assignment";
 import Quiz from "./pages/course/modules/Quiz";
 import Handouts from "./pages/course/modules/Handouts";
 
+function Landing() {
+  return (
+    <div style={{ padding: 32, color: "white" }}>
+      <h1>ScaleShift Learn</h1>
+      <p>Digital Marketing Foundations</p>
+      <a href="/course">👉 Go to Course</a>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/course" replace />} />
+      <Route path="/" element={<Landing />} />
 
       <Route path="/course" element={<CourseHome />} />
       <Route path="/course/module/:id" element={<Module />} />
@@ -17,8 +27,6 @@ export default function App() {
       <Route path="/course/module/:id/assignment" element={<Assignment />} />
       <Route path="/course/module/:id/quiz" element={<Quiz />} />
       <Route path="/course/module/:id/handouts" element={<Handouts />} />
-
-      <Route path="*" element={<Navigate to="/course" replace />} />
     </Routes>
   );
 }
